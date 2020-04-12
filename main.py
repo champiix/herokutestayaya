3d59976d6306a86113cd688d94b4b4b5d36ea046
champii <henrihiio9@gmail.com>
2020-04-12 21:23:59 +0000

    uh

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import asyncio
 from webserver import keep_alive
 import os
 
-client = commands.Bot(command_prefix = ".")
+client = commands.Bot(command_prefix = "~")
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.dnd, activity=discord.Game("uwu"))
+    await client.change_presence(status=discord.Status.dnd, activity=discord.Game("prefix is ~"))
     print("bot is ready")
 
 
@@ -18,14 +18,14 @@ async def ping(ctx):
     await ctx.send(f'pong! {round(client.latency * 1000)}ms')
 
 @client.command(aliases=["8ball"])
-async def _8ball(ctx, *, question):
+async def _8ball(ctx):
     responses = ["yes",
                  "maybe",
                  "my sources say no",
                  "my sources say yes",
                  "no",
                  "bitch tf you crazy?"]
-    await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
+    await ctx.send( f" {random.choice(responses)}, "+ctx.message.author.mention)
 
 @client.command(aliases=["roll"])
 async def dice(ctx):
@@ -43,36 +43,6 @@ async def coinflip(ctx):
                  "tails",]
     await ctx.send(f"{random.choice(responses)}")
 
-@client.command()
-@commands.has_permissions(manage_messages=True)
-async def clear(ctx, amount=0):
-    await ctx.channel.purge(limit=amount + 1)
-
-@client.command()
-@commands.has_permissions(manage_messages=True)
-async def kick(ctx, member : discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(f"kicked {member} for {reason}")
-
-@client.command()
-@commands.has_permissions(manage_messages=True)
-async def ban(ctx, member : discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f"banned {member} for {reason}")
-
-@client.command()
-@commands.has_permissions(manage_messages=True)
-async def mute(ctx, member: discord.Member):
-    role = discord.utils.get(ctx.guild.roles, name='Muted')
-    await member.add_roles(role)
-    await ctx.send(f"{member} muted")
-
-@client.command()
-@commands.has_permissions(manage_messages=True)
-async def unmute(ctx, member: discord.Member):
-    role = discord.utils.get(ctx.guild.roles, name='Muted')
-    await member.remove_roles(role)
-    await ctx.send(f'{member} unmuted')
 
 @client.command()
 async def intro(ctx):
@@ -80,16 +50,13 @@ async def intro(ctx):
 
 @client.command()
 async def logo(ctx):
-  embed = discord.Embed()
+  embed = discord.Embed(title="2d logo")
   embed.set_image(url="https://media.discordapp.net/attachments/483227709992861696/590435949658374170/rage_traced_2d.png?width=474&height=474")
   await ctx.send(embed=embed)
-
-
-@client.command()
-async def logo3d(ctx):
-  embed = discord.Embed()
+  embed = discord.Embed(title="3d logo")
   embed.set_image(url="https://media.discordapp.net/attachments/483227709992861696/590435953466933262/rage_traced_3d.png?width=474&height=474")
   await ctx.send(embed=embed)
+
 
 
 @client.command()
@@ -112,7 +79,6 @@ async def wheelchair(ctx):
     embed = discord.Embed(colour=discord.Colour.red())
     embed.set_image(url="https://cdn.discordapp.com/attachments/684090883158573109/697947452099002378/1586474027404.gif")
     await ctx.send(embed=embed)
-
 
 
 
